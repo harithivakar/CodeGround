517408ba8263aa6136bc17cd8c3aef1bc6fbaaa3
Harithivakar <harithivakar.nadar@gmail.com>
2022-06-01 19:15:05 +0530

    Created server and added configuration

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+
+module.exports = app;
+
---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const app = require('./app.js');
+
+console.time('server');
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on ${process.env.PORT || 3000}`);
+});
+
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {'content-type': 'text/plain'});
+//     res.end('Hello, World !');
+// });
+
+// server.listen(3000, () => {
+//     console.log('Server running on 3000');
+// });
+
+console.timeEnd('server');
+
